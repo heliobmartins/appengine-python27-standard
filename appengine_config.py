bb0c2cd53003495b4a440068cda85cf9a9af4709
Helio Baptista Martins <hbm.helio@gmail.com>
2018-11-27 08:41:17 +1100

    Enabling appstats on console

--- appengine_config.py
+++ appengine_config.py
@@ -1,9 +1,15 @@
 import os
-import google
 
+import google
 from google.appengine.ext import vendor
 
 lib_directory = os.path.dirname(__file__) + '/lib'
 # Change where to find the google package (point to the lib/ directory)
 google.__path__ = [os.path.join(lib_directory, 'google')] + google.__path__
 vendor.add(lib_directory)
+
+
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
