3ca312544c5c1fedf81109951dfead075c3c0c2c
Helio Baptista Martins <hbm.helio@gmail.com>
2019-09-30 14:25:08 +1000

    Added woraround to make compile

--- appengine_config.py
+++ appengine_config.py
@@ -13,3 +13,9 @@ def webapp_add_wsgi_middleware(app):
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
     return app
+
+try:
+    from google.appengine.tools.devappserver2.python.runtime.stubs import FakeFile
+    FakeFile._allowed_dirs.update(['/System/Library/CoreServices/'])
+except ImportError:
+    pass
